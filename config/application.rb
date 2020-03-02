af4b7ff46b5aebe53772dade6b6981389f4958b9
emalvad <emalvad@ericsson.com>
2020-03-02 16:37:04 +0000

    empty app shell with gems

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ end
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    #
+
     config.assets.initialize_on_precompile=false
   end
 end
